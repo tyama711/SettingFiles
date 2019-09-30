e1f3f2e974e37c913d4de9acd83f2ebc72a202fc
Takuya Yamasaki <takuyaya@yahoo-corp.jp>
2019-09-30 11:05:22 +0900

    add emojify and emoji-cli

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -15,6 +15,7 @@ zplug "junegunn/fzf", \
       rename-to:"fzf", \
       hook-build:"./install --key-bindings --completion --no-update-rc --no-bash --no-fish --64", \
       hook-load:"[ -f $HOME/.fzf.zsh ] && source ~/.fzf.zsh"
+FZF_DEFAULT_OPTS="--exact"
 
 zplug "github/hub", \
       from:gh-r, \
@@ -35,13 +36,29 @@ zplug "stedolan/jq", \
 
 zplug "arks22/tmuximum", as:command
 
+zplug "mrowa44/emojify", as:command, use:emojify
+
+zplug "b4b4r07/emoji-cli"
+
+zplug "mollifier/anyframe"
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
 # expressly specify to use fzf
 zstyle ":anyframe:selector:" use fzf
 # specify path and options for peco, percol, or fzf
 zstyle ":anyframe:selector:fzf:" command 'fzf --extended'
-zplug "mollifier/anyframe"
+bindkey '^xb'  anyframe-widget-cdr
+bindkey '^x^b' anyframe-widget-checkout-git-branch
+bindkey '^xr'  anyframe-widget-execute-history
+bindkey '^x^r' anyframe-widget-execute-history
+bindkey '^xi'  anyframe-widget-put-history
+bindkey '^x^i' anyframe-widget-put-history
+bindkey '^xg'  anyframe-widget-cd-ghq-repository
+bindkey '^x^g' anyframe-widget-cd-ghq-repository
+bindkey '^xk'  anyframe-widget-kill
+bindkey '^x^k' anyframe-widget-kill
+bindkey '^xe'  anyframe-widget-insert-git-branch
+bindkey '^x^e' anyframe-widget-insert-git-branchfi
 
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=10"
 zplug "zsh-users/zsh-autosuggestions"
@@ -49,7 +66,10 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
 zplug "zsh-users/zsh-history-substring-search", defer:3
+bindkey '^[p' history-substring-search-up
+bindkey '^[n' history-substring-search-down
 
 if ( is-at-least 5.1 ); then
     zplug "romkatv/powerlevel10k", as:theme
